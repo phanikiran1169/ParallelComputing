d73ef6c9e8acaed4e9c4383cd0c3add6b9b32020
Phani Kiran V <phanikiran1169@gmail.com>
2025-05-01 02:13:12 -0500

    Project 01 - part 2 (final attempt)

--- project01/project01-part02/main.cpp
+++ project01/project01-part02/main.cpp
@@ -25,9 +25,9 @@
 #include <chrono>
 #include <random>
 #include <unordered_set>
-#include <queue>
 #include <sys/sysinfo.h>
 #include <omp.h>
+#include <mutex>
 
 #include "workgraph.h"
 
@@ -38,12 +38,42 @@ using namespace std;
 // Globals:
 //
 static int _numThreads = 1;  // default to sequential execution
+static std::mutex visited_mutex;   
+static std::unordered_set<int> visited;
 
 //
 // Function prototypes:
 //
 static void ProcessCmdLineArgs(int argc, char* argv[]);
 
+// Here we parallelize the BFS traversal using recursion
+// The idea is to visit all reachable vertices in parallel 
+// until we exhaust all the threads
+
+// If the vertex was already visited, immediately return avoiding
+// redundant processing
+
+// Since multiple threads access the visited set, its access must be  
+// protected using a mutex
+void do_work(WorkGraph& wg, int vertex) 
+{
+	// Mutex to prevent concurrent access of visited set
+	// by multiple threads
+	{
+		lock_guard<mutex> lock(visited_mutex);
+        if (!visited.insert(vertex).second)
+            return;
+	}
+
+	// Do work and get the neighbors
+	vector<int> neighbors = wg.do_work(vertex);
+
+	// Recursively do work for neighbors
+	for (int neighbor : neighbors) {
+        #pragma omp task
+        do_work(wg, neighbor);
+    }
+}
 
 //
 // main:
@@ -70,93 +100,14 @@ int main(int argc, char *argv[])
 
   	auto start = chrono::high_resolution_clock::now();
 	
-	// Starting vertex
-	int start_vertex = wg.start_vertex();
-	
-	// Set to track the visited nodes
-	unordered_set<int> visited;
-	visited.insert(start_vertex);
-
-	// Queue for BFS traversal. Stores which nodes shall be processed next
-	queue<int> bfs;
-	bfs.push(start_vertex);
-	
-	// Buffer to keep track of vertices to process for the current iteration
-	vector<int> current_level;
-	
-	// Counter to track the number of processed cells
-	int cells = 0;
-
-	// Size of the current level
-	int current_level_size = 0;
-
-	/*
-		To solve the problem, BFS traversal is used to access all the vertices and the traversal is parallelized.
-		The idea is to 
-			- extract all the vertices that need to be processed at the current level,
-			- perform the required work for each vertex, 
-			- check their unvisited neighbors and,
-			- push the unvisited neighbors into the queue for future processing.
-
-		The work done on the vertices at the current level i.e. calling of do_work
-		(this is the most time-consuming operation) is parallelized
-		Other operations like pushing into the BFS queue or marking vertices as visited are lightweight 
-		and can be handled sequentially.
-
-		The BFS queue, the visited vertices set, and the cell counter are shared resources among multiple threads,
-		they must be protected using critical sections or atomic operations to ensure that updating these 
-		resources is thread safe.
-	*/
-	while(not bfs.empty()) {
-		// Clear the vertices from previous level
-		current_level.clear();
-
-		// Get the number of vertices to process at the current level
-		current_level_size = bfs.size();
-
-		// Get the vertices to be processed into the current level buffer
-		for (int i = 0; i < current_level_size; ++i) {
-			current_level.push_back(bfs.front());
-			bfs.pop();
-		}
-
-		// Parallelizing the visiting of vertices, doing the work, and getting the unvisited neighbors
-		#pragma omp parallel for num_threads(_numThreads) schedule(dynamic)
-		for (int i = 0; i < current_level_size; ++i) {
-			int vertex = current_level[i];
-			vector<int> neighbors = wg.do_work(vertex);
-
-			// Visit each neighbor for the current vertex
-			for (auto neighbor : neighbors) {
-
-				// Flag to check if a vertex was visited
-				bool vertex_visited = false;
-
-				// Critical pragma because visited set and BFS queue are shared
-				// and must be accessed safely by only one thread at a time
-				#pragma omp critical
-				{	
-					// If the neighbor is unvisited, then mark it visited and add it to the queue
-					if (visited.find(neighbor) == visited.end()) {
-						visited.insert(neighbor);
-						bfs.push(neighbor);
-						vertex_visited = true;
-					}
-				}
-
-				// Increment the counter only if a new vertex was visited
-				// otherwise it will show false progress
-				if (vertex_visited) {
-					#pragma omp atomic
-					cells++;
-				}
-
-				// Display progress for every 500 vertices visited
-				if(cells % 500 == 0) {
-					cout << ".";
-					cout.flush();
-				}
-			}
+	// Parallel block with the specified number of threads
+	#pragma omp parallel num_threads(_numThreads)
+	{
+		// Limit to a single thread because the
+		// traversal begins from a single starting vertex
+		#pragma omp single
+		{
+			do_work(wg, wg.start_vertex());
 		}
 	}
 
